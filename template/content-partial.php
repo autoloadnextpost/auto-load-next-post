e4a069ad9c86c1489c78503a832e7791c3c5a71c
Sébastien Dumont <mailme@sebastiendumont.com>
2018-04-17 04:29:19 +0200

    Added spacing in template

--- template/content-partial.php
+++ template/content-partial.php
@@ -16,20 +16,20 @@
  * @version 1.4.10
 */
 
-$template_location = apply_filters('alnp_template_location', ''); // e.g. "template-parts/post/"
+$template_location = apply_filters( 'alnp_template_location', '' ); // e.g. "template-parts/post/"
 
 if ( have_posts() ) :
 
 	// Load content before the loop.
-	do_action('alnp_load_before_loop');
+	do_action( 'alnp_load_before_loop' );
 
 	// Check that there are posts to load.
-	while (have_posts()) : the_post();
+	while ( have_posts() ) : the_post();
 
 		$post_format = get_post_format(); // Post Format e.g. video
 
 		// Load content before the post content.
-		do_action('alnp_load_before_content');
+		do_action( 'alnp_load_before_content' );
 
 		// Load content before the post content for a specific post format.
 		do_action('alnp_load_before_content_type_'.$post_format);
@@ -51,17 +51,17 @@ if ( have_posts() ) :
 		do_action('alnp_load_after_content_type_'.$post_format);
 
 		// Load content after the post content.
-		do_action('alnp_load_after_content');
+		do_action( 'alnp_load_after_content' );
 
 	// End the loop.
 	endwhile;
 
 		// Load content after the loop.
-		do_action('alnp_load_after_loop');
+		do_action( 'alnp_load_after_loop' );
 
 else :
 
 	// Load content if there are no more posts.
-	do_action('alnp_no_more_posts');
+	do_action( 'alnp_no_more_posts' );
 
 endif; // END if have_posts()
