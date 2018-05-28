a743a6a6ab3573f799b12fb451c34c0a10f677fb
Sébastien Dumont <mailme@sebastiendumont.com>
2018-05-28 18:22:06 +0200

    Added theme support for Twenty Fifteen to Seventeen.

---
+++ includes/theme-support/class-alnp-twenty-fifteen.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Auto Load Next Post Theme Support: Twenty Fifteen
+ *
+ * Applies support for WordPress Twenty Fifteen Theme.
+ *
+ * @since    1.5.0
+ * @author   Sébastien Dumont
+ * @category Core
+ * @package  Auto Load Next Post
+ * @license  GPL-2.0+
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * ALNP_Twenty_Fifteen class.
+ */
+class ALNP_Twenty_Fifteen {
+
+	/**
+	 * Initlize Theme.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function init() {
+		// Add theme support and preset the theme selectors.
+		add_action( 'after_setup_theme', array( __CLASS__, 'add_theme_support' ) );
+	} // END init()
+
+	/**
+	 * Add theme support by providing the theme selectors
+	 * to be applied once the theme is activated.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function add_theme_support() {
+		add_theme_support( 'auto-load-next-post', array(
+			'content_container'    => 'main.site-main',
+			'title_selector'       => 'h1.entry-title',
+			'navigation_container' => 'nav.post-navigation',
+			'comments_container'   => 'section#comments',
+		) );
+	} // END add_theme_support()
+
+} // END class
+
+ALNP_Twenty_Fifteen::init();
