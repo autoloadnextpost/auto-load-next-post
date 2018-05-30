ac8b50a3418a34df22b7b828a398928d68024c59
Sébastien Dumont <mailme@sebastiendumont.com>
2018-05-30 19:23:47 +0200

    Make Tested and Supported

---
+++ includes/theme-support/class-alnp-make.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Auto Load Next Post Theme Support: Make
+ *
+ * Applies support for The Theme Foundry Make Theme.
+ *
+ * @since    1.5.0
+ * @author   Sébastien Dumont
+ * @category Core
+ * @package  Auto Load Next Post
+ * @license  GPL-2.0+
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * ALNP_Make class.
+ */
+class ALNP_Make {
+
+	/**
+	 * Initlize Theme.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function init() {
+		// Filters the repeater template location.
+		add_filter( 'alnp_template_redirect', array( __CLASS__, 'alnp_make_template_redirect' ) );
+
+		// Add theme support and preset the theme selectors.
+		add_action( 'after_setup_theme', array( __CLASS__, 'add_theme_support' ) );
+	} // END init()
+
+	/**
+	 * Filters the location of the repeater template.
+	 *
+	 * @access public
+	 * @static
+	 * @return string
+	 */
+	public static function alnp_make_template_redirect() {
+		return AUTO_LOAD_NEXT_POST_FILE_PATH . '/template/theme-support/make/content-alnp.php';
+	} // END alnp_make_template_redirect()
+
+	/**
+	 * Add theme support by providing the theme selectors
+	 * to be applied once the theme is activated.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function add_theme_support() {
+		add_theme_support( 'auto-load-next-post', array(
+			'content_container'    => 'main.site-main',
+			'title_selector'       => 'h1.entry-title',
+			'navigation_container' => 'nav.post-navigation',
+			'comments_container'   => 'div#comments',
+		) );
+	} // END add_theme_support()
+
+} // END class
+
+ALNP_Make::init();
