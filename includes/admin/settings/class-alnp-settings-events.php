8dea9f0d7e61124a937aa137714b5959f7d72eb1
Sébastien Dumont <mailme@sebastiendumont.com>
2018-07-15 20:36:17 +0200

    Minor corrections to the events settings page.

--- includes/admin/settings/class-alnp-settings-events.php
+++ includes/admin/settings/class-alnp-settings-events.php
@@ -2,11 +2,10 @@
 /**
  * Auto Load Next Post Settings - Events
  *
- * @since    1.0.0
- * @version  1.4.10
+ * @since    1.5.0
  * @author   Sébastien Dumont
  * @category Admin
- * @package  Auto Load Next Post
+ * @package  Auto Load Next Post/Admin/Settings
  * @license  GPL-2.0+
  */
 
@@ -28,7 +27,7 @@ if ( ! class_exists( 'Auto_Load_Next_Post_Settings_Events_Tab' ) ) {
 		 */
 		public function __construct() {
 			$this->id    = 'events';
-			$this->label = __( 'Events', 'auto-load-next-post' );
+			$this->label = esc_html__( 'Events', 'auto-load-next-post' );
 
 			parent::__construct();
 		} // END __construct()
@@ -42,40 +41,40 @@ if ( ! class_exists( 'Auto_Load_Next_Post_Settings_Events_Tab' ) ) {
 		 */
 		public function get_settings() {
 			return apply_filters(
-				'auto_load_next_post_general_settings', array(
+				'auto_load_next_post_event_settings', array(
 
 					array(
-						'title' => __( 'Events', 'auto-load-next-post' ),
+						'title' => $this->label,
 						'type'  => 'title',
-						'desc'  => sprintf( __( 'User defined events, below you can choose external JavaScript events to be triggered alongside Auto Load Next Post native events', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ),
+						'desc'  => sprintf( esc_html__( 'User defined events, below you can choose external JavaScript events to be triggered alongside %s native events', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ),
 						'id'    => 'events_options'
 					),
 
 					array(
-						'title'    => __( 'Post loaded', 'auto-load-next-post' ),
-						'desc'     => __( 'After the new post has loaded, comma separated events list: <code>event1,event2, ...</code>', 'auto-load-next-post' ),
-						'id'       => 'auto_load_next_post_on_load_event',
+						'title'       => esc_html__( 'Post loaded', 'auto-load-next-post' ),
+						'desc'        => __( 'After the new post has loaded, comma separated events list: <code>event1, event2, ...</code>', 'auto-load-next-post' ),
+						'id'          => 'auto_load_next_post_on_load_event',
 						'placeholder' => 'event1,event2, ...',
-						'type'     => 'textarea',
-						'css'      => 'min-width:300px;',
-						'autoload' => false
+						'type'        => 'textarea',
+						'css'         => 'min-width:300px;',
+						'autoload'    => false
 					),
 
 					array(
-						'title'    => __( 'Entering a post', 'auto-load-next-post' ),
-						'desc'     => __( 'Entering a post, comma separated events list: <code>event1,event2, ...</code>', 'auto-load-next-post' ),
-						'id'       => 'auto_load_next_post_on_entering_event',
+						'title'       => esc_html__( 'Entering a post', 'auto-load-next-post' ),
+						'desc'        => esc_html__( 'Entering a post, comma separated events list: <code>event1,event2, ...</code>', 'auto-load-next-post' ),
+						'id'          => 'auto_load_next_post_on_entering_event',
 						'placeholder' => 'event1,event2, ...',
-						'type'     => 'textarea',
-						'css'      => 'min-width:300px;',
-						'autoload' => false
+						'type'        => 'textarea',
+						'css'         => 'min-width:300px;',
+						'autoload'    => false
 					),
 
 					array(
 						'type' => 'sectionend',
 						'id'   => 'events_options'
 					),
-			) ); // End general settings
+			) ); // End event settings
 		} // END get_settings()
 
 		/**
@@ -86,6 +85,7 @@ if ( ! class_exists( 'Auto_Load_Next_Post_Settings_Events_Tab' ) ) {
 		 */
 		public function output() {
 			$settings = $this->get_settings();
+
 			Auto_Load_Next_Post_Admin_Settings::output_fields( $settings );
 		} // END output()
 
@@ -98,6 +98,7 @@ if ( ! class_exists( 'Auto_Load_Next_Post_Settings_Events_Tab' ) ) {
 		 */
 		public function save() {
 			$settings = $this->get_settings();
+
 			Auto_Load_Next_Post_Admin_Settings::save_fields( $settings );
 		} // END save()
 
