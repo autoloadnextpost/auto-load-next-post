823750042f15e518beb5b77c1543fa636eb9b060
Sébastien Dumont <mailme@sebastiendumont.com>
2018-08-03 17:28:03 +0200

    Notification styling corrections

--- includes/admin/views/html-notice-no-theme-selectors.php
+++ includes/admin/views/html-notice-no-theme-selectors.php
@@ -14,6 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="notice notice-error auto-load-next-post-message">
+<div class="notice notice-error">
 	<p><?php echo sprintf( __( 'It seems that not all of the required theme selectors have been set. These theme selectors are required in order for %1$s to work. %2$sHow to find my theme selectors?%3$s', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ), '<a href="https://autoloadnextpost.com/documentation/find-theme-selectors/?utm_source=wpadmin&utm_campaign=plugin-settings-theme-selectors" target="_blank">', '</a>' ); ?></p>
 </div>
