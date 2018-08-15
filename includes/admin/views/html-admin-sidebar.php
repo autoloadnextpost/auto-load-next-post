7770067c5f474394ffbd81b3f5f21355598fa13a
Sébastien Dumont <mailme@sebastiendumont.com>
2018-08-15 17:50:29 +0200

    Defined link to load in a new window

--- includes/admin/views/html-admin-sidebar.php
+++ includes/admin/views/html-admin-sidebar.php
@@ -61,7 +61,7 @@
 		<h4><?php esc_html_e( 'Created &amp; maintained by', 'auto-load-next-post' ); ?></h4>
 		<ul>
 			<li>
-				<a href="https://profiles.wordpress.org/sebd86">
+				<a href="https://profiles.wordpress.org/sebd86" target="_blank">
 					<img src="https://secure.gravatar.com/avatar/c3f9be587ccc8656bad714cb945f70a4?size=32" alt="Sébastien Dumont" width="32" height="32">
 					<span>Sébastien Dumont</span>
 				</a>
