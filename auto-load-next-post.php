9fc47561969c432264c8a28825bcbe7997dd1acf
Sébastien Dumont <mailme@sebastiendumont.com>
2018-08-17 13:33:26 +0200

    Bumped version

--- auto-load-next-post.php
+++ auto-load-next-post.php
@@ -5,7 +5,7 @@
  * Description: Increase your pageviews on your site as readers continue reading your posts scrolling down the page.
  * Author:      Sébastien Dumont
  * Author URI:  https://sebastiendumont.com
- * Version:     1.5.0-beta.1
+ * Version:     1.5.0-beta.2
  * Text Domain: auto-load-next-post
  * Domain Path: /languages/
  *
