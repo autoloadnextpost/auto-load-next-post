e8abe872eebbaca9bfb4d5a84129ae0446998027
Sébastien Dumont <mailme@sebastiendumont.com>
2019-03-25 02:12:19 +0100

    Tweak: Congratulations admin notification #154

--- includes/admin/views/html-notice-theme-ready.php
+++ includes/admin/views/html-notice-theme-ready.php
@@ -3,6 +3,7 @@
  * Admin View: Theme Ready Notice.
  *
  * @since    1.5.0
+ * @version  1.5.7
  * @author   Sébastien Dumont
  * @category Admin
  * @package  Auto Load Next Post/Admin/Views
@@ -23,8 +24,8 @@ $active_theme = wp_get_theme();
 		</div>
 
 		<div class="auto-load-next-post-notice-content">
-			<h3><?php echo esc_html__( 'Congratulations!', 'auto-load-next-post' ); ?></h3>
-			<p><?php echo sprintf( __( 'Your current theme <strong>%1$s</strong> is supported. %2$s is ready to increase your pageviews. There is nothing else to setup so your good to go.', 'auto-load-next-post' ), $active_theme->name, esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ); ?></p>
+			<h3><?php echo sprintf( esc_html__( 'Thank you for installing >%1$s!', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ); ?></h3>
+			<p><?php echo sprintf( __( 'We detected your theme <strong>%1$s</strong> already supports %2$s. Everything is already setup for you and is ready to increase your pageviews.', 'auto-load-next-post' ), $active_theme->name, esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ); ?></p>
 		</div>
 	</div>
 </div>
