910453c785ba642bc482753ead58a83851c8ed58
Sébastien Dumont <mailme@sebastiendumont.com>
2019-04-08 21:53:12 +0200

    Tweaked admin tab links

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -28,7 +28,12 @@ if ( ! $tab_exists ) {
 		<nav class="nav-tab-wrapper">
 			<?php
 				foreach ( $tabs as $slug => $label ) {
-					echo '<a href="' . esc_html( admin_url( 'options-general.php?page=auto-load-next-post-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '">' . esc_html( $label ) . '</a>';
+					$url = add_query_arg( array(
+						'page' => 'auto-load-next-post-settings',
+						'tab'  => esc_attr( $slug ),
+					), admin_url( 'options-general.php' ) );
+
+					echo '<a href="' . esc_html( $url ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '">' . esc_html( $label ) . '</a>';
 				}
 
 				do_action( 'auto_load_next_post_settings_tabs' );
