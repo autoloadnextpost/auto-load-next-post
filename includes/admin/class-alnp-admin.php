fb18b8c3ca82077d9359166a926c39169b1c41a9
Sébastien Dumont <mailme@sebastiendumont.com>
2019-04-08 22:17:17 +0200

    Moved admin notices outside of ajax condition

--- includes/admin/class-alnp-admin.php
+++ includes/admin/class-alnp-admin.php
@@ -54,6 +54,8 @@ if ( ! class_exists( 'Auto_Load_Next_Post_Admin' ) ) {
 		 * @since  1.0.0
 		 */
 		public function includes() {
+			include( dirname( __FILE__ ) . '/class-alnp-admin-notices.php' ); // Plugin Notices
+
 			// Classes we only need if the ajax is not-ajax
 			if ( ! auto_load_next_post_is_ajax() ) {
 				include( dirname( __FILE__ ) . '/class-alnp-admin-notices.php' ); // Plugin Notices
