fb1d9e3dff723f8e012ab26de01a457691517f77
Sébastien Dumont <mailme@sebastiendumont.com>
2019-05-03 13:16:55 +0200

    Only load required assets when needed

--- includes/admin/class-alnp-admin-assets.php
+++ includes/admin/class-alnp-admin-assets.php
@@ -47,15 +47,20 @@ if ( ! class_exists( 'ALNP_Admin_Assets' ) ) {
 			if ( in_array( $screen_id, alnp_get_admin_screens() ) ) {
 				Auto_Load_Next_Post::load_file( AUTO_LOAD_NEXT_POST_SLUG . '_admin', '/assets/css/admin/auto-load-next-post' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.css' );
 
-				Auto_Load_Next_Post::load_file( AUTO_LOAD_NEXT_POST_SLUG . '_getting_started', '/assets/css/admin/getting-started' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.css' );
+				$current_view = ! empty( $_GET['view'] ) ? sanitize_title( wp_unslash( $_GET['view'] ) ) : '';
 
-				// Select2 - Make sure that we remove other registered Select2 to prevent styling issues.
-				if ( wp_script_is( 'select2', 'registered' ) ) {
-					wp_dequeue_style( 'select2' );
-					wp_deregister_style( 'select2' );
+				// Dont load stylesheet if viewing any of these pages.
+				$dont_style = array( 'getting-started', 'setup-wizard', 'extensions', 'videos' );
+				if ( ! in_array( $current_view, $dont_style ) ) {
+
+					// Select2 - Make sure that we remove other registered Select2 to prevent styling issues.
+					if ( wp_script_is( 'select2', 'registered' ) ) {
+						wp_dequeue_style( 'select2' );
+						wp_deregister_style( 'select2' );
+					}
+
+					Auto_Load_Next_Post::load_file( 'select2', '/assets/css/libs/select2' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.css' );
 				}
-
-				Auto_Load_Next_Post::load_file( 'select2', '/assets/css/libs/select2' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.css' );
 			}
 		} // END admin_styles()
 
@@ -69,24 +74,36 @@ if ( ! class_exists( 'ALNP_Admin_Assets' ) ) {
 			$screen_id = $screen ? $screen->id : '';
 
 			if ( $screen_id == 'settings_page_auto-load-next-post' ) {
-				// Select2 - Make sure that we remove other registered Select2 to prevent plugin conflict issues.
-				if ( wp_script_is( 'select2', 'registered' ) ) {
-					wp_dequeue_script( 'select2' );
-					wp_deregister_script( 'select2' );
+
+				$current_view = ! empty( $_GET['view'] ) ? sanitize_title( wp_unslash( $_GET['view'] ) ) : '';
+
+				switch( $current_view ) {
+					case 'setup-wizard':
+						// Scanner.
+						Auto_Load_Next_Post::load_file( AUTO_LOAD_NEXT_POST_SLUG . '_admin', '/assets/js/admin/scanner' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.js', true, array( 'jquery' ), AUTO_LOAD_NEXT_POST_VERSION, true );
+					break;
+					default:
+						// Select2 - Make sure that we remove other registered Select2 to prevent plugin conflict issues.
+						if ( wp_script_is( 'select2', 'registered' ) ) {
+							wp_dequeue_script( 'select2' );
+							wp_deregister_script( 'select2' );
+						}
+
+						// Load Select2
+						Auto_Load_Next_Post::load_file( 'select2', '/assets/js/libs/select2' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.js', true, array( 'jquery' ), '4.0.5', true );
+
+						// Load plugin settings.
+						Auto_Load_Next_Post::load_file( AUTO_LOAD_NEXT_POST_SLUG . '_admin', '/assets/js/admin/settings' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.js', true, array( 'jquery' ), AUTO_LOAD_NEXT_POST_VERSION, true );
+
+						// Variables for Admin JavaScript.
+						wp_localize_script( AUTO_LOAD_NEXT_POST_SLUG . '_admin', 'alnp_settings_params', array(
+							'is_rtl'             => is_rtl() ? 'rtl' : 'ltr',
+							'i18n_nav_warning'   => esc_html__( 'The changes you made will be lost if you navigate away from this page.', 'auto-load-next-post' ),
+							'i18n_reset_warning' => sprintf( esc_html__( 'This will reset all settings back to default and re-initialize %s. Are you sure?', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ),
+						) );
+					break;
 				}
-
-				// Load Select2
-				Auto_Load_Next_Post::load_file( 'select2', '/assets/js/libs/select2' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.js', true, array( 'jquery' ), '4.0.5', true );
-
-				// Load plugin settings.
-				Auto_Load_Next_Post::load_file( AUTO_LOAD_NEXT_POST_SLUG . '_admin', '/assets/js/admin/settings' . AUTO_LOAD_NEXT_POST_SCRIPT_MODE . '.js', true, array( 'jquery' ), AUTO_LOAD_NEXT_POST_VERSION, true );
-
-				// Variables for Admin JavaScript.
-				wp_localize_script( AUTO_LOAD_NEXT_POST_SLUG . '_admin', 'alnp_settings_params', array(
-					'is_rtl'             => is_rtl() ? 'rtl' : 'ltr',
-					'i18n_nav_warning'   => esc_html__( 'The changes you made will be lost if you navigate away from this page.', 'auto-load-next-post' ),
-					'i18n_reset_warning' => sprintf( esc_html__( 'This will reset all settings back to default and re-initialize %s. Are you sure?', 'auto-load-next-post' ), esc_html__( 'Auto Load Next Post', 'auto-load-next-post' ) ),
-				) );
+	
 			}
 		} // END admin_scripts()
 
