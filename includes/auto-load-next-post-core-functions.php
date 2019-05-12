d18d51e6ab6b65101cca22c509a312ddc21d7e9a
Sébastien Dumont <mailme@sebastiendumont.com>
2019-05-12 19:05:28 +0200

    Wrapped function exists around inline documenation

--- includes/auto-load-next-post-core-functions.php
+++ includes/auto-load-next-post-core-functions.php
@@ -17,6 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+if ( ! function_exists( 'alnp_get_random_page_permalink' ) ) {
 /**
  * Returns the permalink of a random page
  *
@@ -24,7 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @param  string $post_type - Default is post.
  * @return int|boolean
  */
-if ( ! function_exists( 'alnp_get_random_page_permalink' ) ) {
 	function alnp_get_random_page_permalink( $post_type = 'post' ) {
 		$args = array(
 			'post_type'      => $post_type,
@@ -47,13 +47,13 @@ if ( ! function_exists( 'alnp_get_random_page_permalink' ) ) {
 	} // END alnp_get_random_page_permalink()
 }
 
+if ( ! function_exists( 'alnp_load_js_in_footer' ) ) {
 /**
  * This helps the plugin decide to load the JavaScript in the footer or not.
  * 
  * @since  1.5.7
  * @return boolean
  */
-if ( ! function_exists( 'alnp_load_js_in_footer' ) ) {
 	function alnp_load_js_in_footer() {
 		$load_in_footer = get_option( 'auto_load_next_post_load_js_in_footer', false );
 
@@ -65,6 +65,7 @@ if ( ! function_exists( 'alnp_load_js_in_footer' ) ) {
 	} // END alnp_load_js_in_footer()
 }
 
+if ( ! function_exists( 'alnp_get_admin_screens' ) ) {
 /**
  * These are the only screens Auto Load Next Post will focus 
  * on displaying notices or equeue scripts/styles.
@@ -73,7 +74,6 @@ if ( ! function_exists( 'alnp_load_js_in_footer' ) ) {
  * @version 1.6.0
  * @return  array
  */
-if ( ! function_exists( 'alnp_get_admin_screens' ) ) {
 	function alnp_get_admin_screens() {
 		return array(
 			'dashboard',
@@ -84,12 +84,12 @@ if ( ! function_exists( 'alnp_get_admin_screens' ) ) {
 	} // END alnp_get_admin_screens()
 }
 
+if ( ! function_exists( 'alnp_meta_version' ) ) {
 /**
  * Adds the plugin version to the header.
  *
  * @since 1.6.0
  */
-if ( ! function_exists( 'alnp_meta_version' ) ) {
 	function alnp_meta_version() {
 		echo '<meta name="generator" content="Auto Load Next Post ' . esc_attr( AUTO_LOAD_NEXT_POST_VERSION ) . '" />' . "\n";
 	} // END alnp_meta_version()
