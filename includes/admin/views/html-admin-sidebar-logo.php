54c31ae1d149881de28029a8031c4d491cf111bd
Sébastien Dumont <mailme@sebastiendumont.com>
2019-05-23 18:18:19 +0200

    Logo in sidebar now responsive

--- includes/admin/views/html-admin-sidebar-logo.php
+++ includes/admin/views/html-admin-sidebar-logo.php
@@ -14,4 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<a class="alnp-banner" href="https://autoloadnextpost.com/?utm_source=plugin&utm_medium=alnp-banner&utm_campaign=alnp-settings-page"></a>
+<a class="alnp-banner" href="https://autoloadnextpost.com/?utm_source=plugin&utm_medium=alnp-banner&utm_campaign=alnp-settings-page">
+	<img src="<?php echo AUTO_LOAD_NEXT_POST_URL_PATH . '/assets/images/banner.png'; ?>" alt="<?php _e( 'Auto Load Next Post', 'auto-load-next-post' ); ?>" width="100%" />
+</a>
